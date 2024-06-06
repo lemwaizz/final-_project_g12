e6874e9807eb24025e679b2df8fae1a0c1dc82c0
JohnOngeri <144005852+JohnOngeri@users.noreply.github.com>
2024-06-06 10:22:12 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+db = SQLAlchemy(app)
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
-    db = SQLAlchemy(app)
+    #db = SQLAlchemy(app)
     migrate = Migrate(app, db)
     login_manager = LoginManager(app)
     login_manager.login_view = 'auth.login'

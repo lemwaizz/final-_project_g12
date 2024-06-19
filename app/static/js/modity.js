af35978a75a555ce9d8ab8bb9678900f653874d3
Kanisa1 <144110428+Kanisa1@users.noreply.github.com>
2024-06-19 11:45:55 +0200

    Add files via upload

---
+++ app/static/js/modity.js
@@ -0,0 +1,25 @@
+function names(){ document.getElementById("Collins").innerHTML="<h1>Collins</h1> <p>Developed and executed unit tests covering critical functionalities.<br>Achieved high code coverage with comprehensive test cases.<br>Integrated tests into the CI/CD pipeline to ensure automated testing on each commit.<br>Monitored test results and worked with developers to fix any issues.</p>";
+
+}
+
+
+function him(){ document.getElementById("Dennis").innerHTML="<h1>Dennis</h1><p>Set up the Flask framework and project structure<br>.Configured SQLAlchemy with PostgreSQL database.<br>Developed and tested RESTful APIs for user registration, login, scheduling, and tracking.<br>Implemented CI/CD pipeline with GitHub Actions.<br>Managed deployments using Fabric.</p>";
+
+}
+
+
+
+
+function he(){ document.getElementById("Johnny").innerHTML="<h1>Johnny</h1> <p>Designed and created database schema in PostgreSQL.<br>Implemented data models using SQLAlchemy.<br>Developed algorithms for optimal scheduling and route management.<br>Ensured efficient database queries and indexing for performance.</p>";
+
+}
+
+function her(){ document.getElementById("Kanisa").innerHTML="<h1>Kanisa</h1> <p>Created responsive UI designs using HTML, CSS, and JavaScript.<br>Used Bootstrap to ensure a modern and user-friendly interface.<br>Developed user interfaces for household users, waste collection services, and administrators.<br>Integrated frontend with backend APIs for dynamic data rendering.</p>";
+
+}
+
+
+function tom(){ document.getElementById("Kay").innerHTML="<h1>Kay</h1> <p>Designed intuitive navigation flows and user interactions.<br>Conducted usability testing and made improvements based on feedback.<br>Created comprehensive project documentation, covering setup, usage, and API endpoints.<br>Prepared user manuals and guides to assist users in understanding the system.</p>"
+
+
+}

ffb6846349da5b447c3fab536a327c6b915e1e00
Kay Armand <144005933+Armandkay@users.noreply.github.com>
2024-06-19 19:34:27 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 # app/forms.py
-#start
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError

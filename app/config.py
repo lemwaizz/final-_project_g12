41a9d980044fe2c9f6f63212407798769bbacabc
lemwaizz <mwaidennis26@gmail.com>
2024-06-19 19:38:14 +0200

    fixed

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://uerc0sc865i3pi:p56c4181667363c1e9b7d1505af1386e8819073cb3f8771419d172f6a8a9bf913@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfhg82te2eb5m3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'my_jwt_secret_key')

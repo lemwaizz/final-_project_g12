442e2603ce6200adb6f8d4f8e69591749eafc921
lemwaizz <mwaidennis26@gmail.com>
2024-06-20 18:12:55 +0200

    check

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
-# app/__init__.py
-
+import os
 from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
-from .routes import register_routes
 
 db = SQLAlchemy()
 
@@ -19,3 +17,9 @@ def create_app():
     app.register_blueprint(auth_bp)
 
     return app
+
+if __name__ == "__main__":
+    # Use the PORT environment variable if provided, otherwise default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app = create_app()
+    app.run(host='0.0.0.0', port=port)

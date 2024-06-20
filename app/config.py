a083e9ca27643612e710df1aa0d07695d7372120
lemwaizz <mwaidennis26@gmail.com>
2024-06-20 20:19:00 +0200

    done

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')  # Replace with your secret key
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost/smart'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/smart'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

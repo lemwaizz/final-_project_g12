c7788f28ef7737692b4fb617e6a297812b7a1d82
lemwaizz <mwaidennis26@gmail.com>
2024-06-20 20:01:59 +0200

    check

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://uerc0sc865i3pi:p56c4181667363c1e9b7d1505af1386e8819073cb3f8771419d172f6a8a9bf913@c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfhg82te2eb5m3')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')  # Replace with your secret key
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost/smart'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_default_jwt_key')

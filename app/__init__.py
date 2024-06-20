be88f1abcd31915ff13dc3fdf141f9a39de7caeb
lemwaizz <mwaidennis26@gmail.com>
2024-06-20 18:34:39 +0200

    check

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,3 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    # Use the PORT environment variable if provided, otherwise default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app = create_app()
-    app.run(host='0.0.0.0', port=port)

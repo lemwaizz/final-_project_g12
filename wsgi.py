4c58290d2ce6d5274a412061644ac3ff04b295ac
lemwaizz <mwaidennis26@gmail.com>
2024-06-20 21:48:38 +0200

    check

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
+# manage.py
+
 from app import create_app
+import os
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5001))  # Use port 5001 for Flask app
+    app.run(host='0.0.0.0', port=port)
